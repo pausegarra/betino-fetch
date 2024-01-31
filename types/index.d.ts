6fd13c1e76b293b7a087f0df8c832ec9d1d27e79
psegarra <pausegarra.b@gmail.com>
2024-01-31 08:55:48 +0100

    🐛 add generic to types

--- types/index.d.ts
+++ types/index.d.ts
@@ -9,28 +9,31 @@ declare module '@betino/fetch' {
 
     constructor(baseURL: string);
 
-    get(
+    get<T = any>(
       endpoint: string,
       params?: Record<string, string>,
       headers?: Record<string, string>
-    ): Promise<FetchResponse>;
+    ): Promise<T>;
 
-    post(
+    post<T = any>(
       endpoint: string,
       body?: any,
       headers?: Record<string, string>
-    ): Promise<FetchResponse>;
+    ): Promise<T>;
 
-    put(
+    put<T = any>(
       endpoint: string,
       body?: any,
       headers?: Record<string, string>
-    ): Promise<FetchResponse>;
+    ): Promise<T>;
 
-    delete(
+    put<T = any>(
       endpoint: string,
+      body?: any,
       headers?: Record<string, string>
-    ): Promise<FetchResponse>;
+    ): Promise<T>;
+
+    delete<T = any>(endpoint: string, headers?: Record<string, string>): Promise<T>;
 
     handleResponse<T>(response: Response): Promise<FetchResponse<T>>;
   }
